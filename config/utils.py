24f45cd71db9be717266e8aa7bbcdca2c121907c
NS <alijeyar765@gmail.com>
2023-02-26 23:55:43 +0300

    refactor utils by separating some code

--- config/utils.py
+++ config/utils.py
@@ -1,13 +1,21 @@
 facebook_url = "https://www.facebook.com/watch/"
 
-def sanitizing_url(url: str) -> str:
+def get_url_id(url: str) -> str:
     url_split = url.split("/")
-    if url_split[len(url_split)-1] == "":
-        id = url_split[len(url_split)-2]
-    else:
-        id = url_split[len(url_split)-1]
-    if id[0] != "?":
-        return facebook_url + "?v=" + id
+    if url_split[len(url_split) - 1] == "":
+        return url_split[len(url_split) - 2]
+    return url_split[len(url_split) - 1]
+
+
+def sanitizing_url(url: str) -> str:
+    id = get_url_id(url)
     if len(id) > 19 and id[0] != "w":
+        print(id[:19])
         return facebook_url + id[:19]
+    if len(id) > 19 and id[0] == "w":
+        print(id[5:])
+        return facebook_url + id[6:]
+    if id[0] != "?":
+        print(id)
+        return facebook_url + "?v=" + id
     return url
