89efc965cd2de83bbb1a5bdbf314bbd0dd2869bf
NS <alijeyar765@gmail.com>
2023-03-17 03:41:32 +0300

    try new headers

--- project/views.py
+++ project/views.py
@@ -20,6 +20,10 @@ FACEBOOK_URL = "https://mbasic.facebook.com/watch/?v="
 embed_controller = Router(tags=["Embed Controller"])
 
 headers = {
+    "User-Agent":"facebookexternalhit/1.1",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language":"en-us,en;q=0.5",
+    "Sec-Fetch-Mode":"navigate",
     "authority": "www.facebook.com",
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language": "en-US,en;q=0.9",
@@ -28,7 +32,7 @@ headers = {
     "upgrade-insecure-requests": "1",
     "referer": "https://www.facebook.com/",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "viewport-width": "1280",
+    "viewport-width": "1280"
 }
 # ----------------- video routes ----------------- #
 @embed_controller.get("/watch")
