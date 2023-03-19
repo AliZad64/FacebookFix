bc26f25a7c57a0fcc25ab034616c71f27ac0773a
NS <alijeyar765@gmail.com>
2023-03-19 04:32:48 +0300

    test only SECRET_KEY env

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = ['fix-facebook.azurewebsites.net']
 
 
 # Application definition
