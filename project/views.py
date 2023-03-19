3f0edb78973890c761d9dddf8c2f170b7a2e735d
AliZad64 <78386181+AliZad64@users.noreply.github.com>
2023-03-20 02:58:07 +0300

    change video res to sd and pick default height and width

--- project/views.py
+++ project/views.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from ninja import Router
 from django.utils.safestring import mark_safe
 from lxml import html
-from config.utils.video import embed_video
+from config.utils.video import embed_video, embed_reel
 
 logger = logging.getLogger(__name__)
 
@@ -37,36 +37,17 @@ def get_video_by_query_param(request, v: str):
     url = FACEBOOK_QUERY_URL + v
     return render(request, "base.html", embed_video(url))
 
-@embed_controller.get("watch/{link_id}")
-@embed_controller.get("reel/{link_id}")
-def get_video_by_id(request, link_id: str,  v: str = None):
-    print(request.path)
-    if v:
-        url = f"https://www.facebook.com/watch?v={link_id}"
-    else:
-        url = f"https://www.facebook.com" + mark_safe(request.path)
-    return render(request, "base.html", embed_video(url))
-
-
 
 @embed_controller.get("watch/{link_id}")
 @embed_controller.get("watch/{link_id}/")
 @embed_controller.get("reel/{link_id}")
 def get_video_by_id(request, link_id: str):
     url = FACEBOOK_URL + mark_safe(request.path)
-    return render(request, "base.html", embed_video(url))
-
-@embed_controller.get("watch/{link_id}")
-@embed_controller.get("reel/{link_id}")
-def get_video_by_id(request, link_id: str,  v: str = None):
-    print(request.path)
-    if v:
-        url = f"https://www.facebook.com/watch?v={link_id}"
+    if "reel" in request.path:
+        result = embed_reel(url)
     else:
-        url = f"https://www.facebook.com" + mark_safe(request.path)
-    return render(request, "base.html", embed_video(url))
-
-
+        result = embed_video(url)
+    return render(request, "base.html", result)
 
 @embed_controller.get("{user}/video/{link_id}")
 @embed_controller.get("{user}/video/{link_id}/")
