7d7e13503d799f6eec8f8233ff3e28eb9eb73b24
NS <alijeyar765@gmail.com>
2024-06-01 17:16:31 +0300

    add regex videos constants

---
+++ constants/constants.regex.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	VIDEO_MEDIA_REGEX = `{"comet_sections":{"message":{"__typename":"CometFeedStoryDefaultMessageRenderingStrategy".*?"collaborators":`
+	VIDEO_HD_REGEX    = `"browser_native_hd_url":"(.*?)"`
+	VIDEO_SD_REGEX    = `"browser_sd_url":"(.*?)"`
+)
