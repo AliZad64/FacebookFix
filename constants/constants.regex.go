da26347626eea42ab3133225d9f74b0de6c6366f
NS <alijeyar765@gmail.com>
2024-06-01 22:42:19 +0300

    add reel regex

--- constants/constants.regex.go
+++ constants/constants.regex.go
@@ -4,4 +4,5 @@ const (
 	VIDEO_MEDIA_REGEX = `{"comet_sections":{"message":{"__typename":"CometFeedStoryDefaultMessageRenderingStrategy".*?"collaborators":`
 	VIDEO_HD_REGEX    = `"browser_native_hd_url":"(.*?)"`
 	VIDEO_SD_REGEX    = `"browser_sd_url":"(.*?)"`
+	REEL_MEDIA_REGEX  = `{"short_form_video_context":{"self_view_boost":.*?},"extensions"`
 )
