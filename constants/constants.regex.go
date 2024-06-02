cf96368ca827710a0a85a04ae5658d243d2bcdc8
NS <alijeyar765@gmail.com>
2024-06-02 03:31:06 +0300

    add marketPlace regex

--- constants/constants.regex.go
+++ constants/constants.regex.go
@@ -7,4 +7,5 @@ const (
 	REEL_MEDIA_REGEX  = `{"short_form_video_context":{"self_view_boost":.*?},"extensions"`
 	PHOTO_MEDIA_REGEX = `({"__bbox":{"complete".+?{"__typename":"CometFeedStoryActorPhotoStrategy".+?"sequence_number":)`
 	PHOTO_REGEX       = `({"data":{"currMedia".+?"is_final".+?}})`
+	MARKET_REGEX      = `("adp_MarketplacePDPContainerQueryRelayPreloader_[0-9a-f]{23}".+?{"__bbox":{.+?"sequence_number":)`
 )
