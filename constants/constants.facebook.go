1ed4cd1c341e0dd9543b34954d7226061336ee6e
NS <alijeyar765@gmail.com>
2024-06-02 03:31:19 +0300

    add marketPlace facebook url

--- constants/constants.facebook.go
+++ constants/constants.facebook.go
@@ -5,4 +5,5 @@ const (
 	VideoURL    = "https://www.facebook.com/watch/?v="
 	ReelURL     = "https://www.facebook.com/reel/"
 	PhotoURL    = "https://www.facebook.com/photo/?fbid="
+	MarketUrl   = "https://www.facebook.com/marketplace/item/"
 )
